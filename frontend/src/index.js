50aaf8ab7fada90a6c8deaef307245e491b18dc8
tnrdd <tnrdd@tutanota.com>
2022-03-17 12:19:48 +0000

    use BrowserHistory of react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./app.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/*  <BrowserRouter>
+    <BrowserRouter>
       <App />
-    </BrowserRouter>*/}
-      <HashRouter>
-      <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
