63c70102b997dbcf165adadbcfa343a43c3a301d
tnrdd <tnrdd@tutanota.com>
2022-03-21 15:24:48 +0000

    add webpack's split chunk optimization

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,66 +1,67 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack")
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-	mode: "development",
+  mode: "development",
   entry: {
-		index: ["./src/index.js"],
+    index: ["./src/index.js"],
   },
-	plugins: [
-
+  plugins: [
     new HtmlWebpackPlugin({
-
       title: "Social Media App",
-			template: "./src/index.html",
+      template: "./src/index.html",
       favicon: "./src/assets/favicon.ico",
-
     }),
     new Dotenv(),
-
   ],
 
-	devtool: "inline-source-map",
+  devtool: "inline-source-map",
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-		clean: true,
+    clean: true,
   },
 
   module: {
-
     rules: [
-
       {
-
         test: /\.css$/i,
 
         use: ["style-loader", "css-loader"],
-
       },
-			{
-
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
 
         type: "asset/resource",
-
       },
-      { test: /\.(js|jsx)$/,
-        
+      {
+        test: /\.(js|jsx)$/,
+
         exclude: /node_modules/,
-        
+
         use: ["babel-loader"],
       },
-
     ],
-
   },
   resolve: {
     extensions: ["*", ".js", ".jsx"],
   },
-  
+
   performance: {
     maxAssetSize: 800000,
     maxEntrypointSize: 800000,
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
 };
